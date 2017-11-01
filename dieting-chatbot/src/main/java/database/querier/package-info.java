a8ad1bff9b25d894ec238e6d43985fc76f1823f2
mcding <mcding@connect.ust.hk>
2017-11-01 21:16:30 +0800

    add SerializeKeeper class and HistKeeper, LogKeeper

---
+++ dieting-chatbot/src/main/java/database/querier/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * package database.querier
+ * This package holds the I/O classes for long lasting information.
+ * FoodQuerier manipulate food nutrition information.
+ * UserQuerier manipulate user information.
+ * HistKeeper and LogQuerier manage the long lasting caching information.
+ * @author mcding
+ * @version 1.1
+ */
+package database.querier;
