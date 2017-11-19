74826ffd3950469bfce9d095df852793af1b676d
Guo <wguoaa@connect.ust.hk>
2017-11-19 15:14:42 +0800

    test

--- dieting-chatbot/src/main/java/misc/package-info.java
+++ dieting-chatbot/src/main/java/misc/package-info.java
@@ -1,7 +1,7 @@
 /**
  * Package misc is the package for miscellaneous functions of the system.
  * It contains classes for user initial input and feedback generation.
- * @author cliubf, szhouan
+ * @author cliubf, szhouan, mcding, wguoaa
  * @version v1.2.0
  */
 package misc;
