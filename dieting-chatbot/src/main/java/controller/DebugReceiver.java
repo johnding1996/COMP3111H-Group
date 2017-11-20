ddd5ba33bafc7500be3cd0477dd3a0aa5423af69
Guo <wguoaa@connect.ust.hk>
2017-11-20 19:09:06 +0800

    add part of javadoc missing

--- dieting-chatbot/src/main/java/controller/DebugReceiver.java
+++ dieting-chatbot/src/main/java/controller/DebugReceiver.java
@@ -26,6 +26,9 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @Service
 public class DebugReceiver implements Consumer<Event<MessageJSON>> {
+    /**
+     * THis is a private attribute EvnetBus handling event.
+     */
     @Autowired
     private EventBus eventBus;
 
